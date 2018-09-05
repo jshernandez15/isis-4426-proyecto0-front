77289e631a8fd10e873f1f4ac2541da778ec5224
Juan Hernández <hernanjua@colpatria.com>
2018-09-05 18:57:24 -0500

    videos en vista detalles

--- src/app/competition-detail/competition-detail.component.ts
+++ src/app/competition-detail/competition-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Competition } from '../model/competition.model';
 import { CompetitionService } from '../service/competition.service';
+import { VideoService } from '../service/video.service';
+import { Video } from '../model/video.model';
 
 @Component({
   selector: 'app-competition-detail',
@@ -12,11 +14,15 @@ export class CompetitionDetailComponent implements OnInit {
 
   competition: Competition;
 
+  videos: Video[];
+
   private sub: any;
 
-  constructor(private competitionService: CompetitionService, private router: ActivatedRoute) { }
+  constructor(private videoService: VideoService, private competitionService: CompetitionService, 
+    private router: ActivatedRoute) { }
 
   ngOnInit() {
+    this.videos = [];
     this.sub = this.router.params.subscribe(params => {
       this.loadCompetition(params['id']);
     });
@@ -24,7 +30,15 @@ export class CompetitionDetailComponent implements OnInit {
 
   private loadCompetition(id: number): void {
     this.competitionService.getCompetitionById(id)
-          .subscribe(competition => this.competition = competition);
+          .subscribe(competition => {
+            this.competition = competition;
+            this.loadVideos(competition.id);
+          });
+  }
+  
+  private loadVideos(competitionId: number): void {
+    this.videoService.getVideos(competitionId)
+          .subscribe(videos => this.videos = videos);
   }
 
   ngOnDestroy() {
