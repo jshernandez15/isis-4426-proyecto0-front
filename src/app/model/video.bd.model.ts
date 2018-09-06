3ec362d91b42a28dc0efbf20ceb62916e4fc3e1c
Cristian Sepulveda <sepulvec@colpatria.com>
2018-09-06 12:26:11 -0500

    update list videos in admin

--- src/app/model/video.bd.model.ts
+++ src/app/model/video.bd.model.ts
@@ -1,19 +1,19 @@
-
 export class VideoBd {
-    constructor(
-        public id_video: number,
-        public name: string = '',
-        public last_name: string = '',
-        public email: string = '',
-        public path_real: string = '',
-        public path_convertido: string = '',
-        public description: string = '',
-        public idConcurso: number,
-        public state_video: string,
-    ) { }
+  constructor(
+    public id_video: number,
+    public name: string = "",
+    public last_name: string = "",
+    public email: string = "",
+    public path_real: string = "",
+    public path_convertido: string = "",
+    public description: string = "",
+    public idConcurso: number,
+    public state_video: string,
+    public created: Date
+  ) {}
 
-    static empty() {
-        let empty = ''
-        return new this(0, '', '', '', '', '', '', 0, '');
-    }
+  static empty() {
+    let empty = "";
+    return new this(0, "", "", "", "", "", "", 0, "", null);
+  }
 }
