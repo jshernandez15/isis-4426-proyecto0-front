4144df65a42ed6607774d4c8006548866f6ebd71
Cristian Sepulveda <sepulvec@colpatria.com>
2018-09-25 12:20:36 -0500

    fix bug and remove code

--- src/app/competition/competition.component.ts
+++ src/app/competition/competition.component.ts
@@ -1,45 +1,20 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator, MatTableDataSource } from '@angular/material';
-import { Router } from '@angular/router';
-import swal from 'sweetalert';
-import { Competition } from '../model/competition.model';
-import { CompetitionService } from '../service/competition.service';
-import { StatusCreateComponent } from '../status-create/status-create.component';
-
-
-
-
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatPaginator, MatTableDataSource } from "@angular/material";
+import { Router } from "@angular/router";
+import swal from "sweetalert";
+import { Competition } from "../model/competition.model";
+import { CompetitionService } from "../service/competition.service";
+import { StatusCreateComponent } from "../status-create/status-create.component";
 
 @Component({
-  selector: 'app-competition',
-  templateUrl: './competition.component.html',
-  styleUrls: ['./competition.component.css']
+  selector: "app-competition",
+  templateUrl: "./competition.component.html",
+  styleUrls: ["./competition.component.css"]
 })
 export class CompetitionComponent implements OnInit {
-
   competitions: Competition[] = [];
 
-  displayedColumns: string[] = ['name', 'index', 'address', 'prize'];
+  displayedColumns: string[] = ["name", "index", "address", "prize"];
 
   dataSource;
 
@@ -47,21 +22,22 @@ export class CompetitionComponent implements OnInit {
 
   showNew: Boolean = false;
 
-  submitType: string = 'Guardar';
+  submitType: string = "Guardar";
 
   selectedRow: number;
 
-  modalities: string[] = ['Presencial', 'Virtual'];
-
+  modalities: string[] = ["Presencial", "Virtual"];
 
   @ViewChild(StatusCreateComponent)
   statusCreateComponent: StatusCreateComponent;
 
-  constructor(private competitionService: CompetitionService, private router: Router) {
-  }
+  constructor(
+    private competitionService: CompetitionService,
+    private router: Router
+  ) {}
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
 
   ngOnInit() {
     this.competitionService.getCompetitions();
@@ -69,18 +45,18 @@ export class CompetitionComponent implements OnInit {
   }
 
   private loadList(): void {
-    this.competitionService.getCompetitions()
-      .subscribe(competitions => {
-        this.competitions = competitions;
-        this.dataSource = new MatTableDataSource<Competition>(this.competitions);
-        this.dataSource.paginator = this.paginator;
-        console.log(this.dataSource);
-      });
+    this.competitionService.getCompetitions().subscribe(competitions => {
+      this.competitions = competitions;
+      this.dataSource = new MatTableDataSource<Competition>(this.competitions);
+      this.dataSource.paginator = this.paginator;
+      console.log(this.dataSource);
+      console.log(this.paginator);
+    });
   }
 
   onNew() {
     this.competitionModel = Competition.empty();
-    this.submitType = 'Guardar';
+    this.submitType = "Guardar";
     this.showNew = true;
   }
 
@@ -92,7 +68,10 @@ export class CompetitionComponent implements OnInit {
       this.competitionModel.banner = this.statusCreateComponent.serverResponse.path;
     }
 
-    if (this.competitionModel.end == null || this.competitionModel.init == null) {
+    if (
+      this.competitionModel.end == null ||
+      this.competitionModel.init == null
+    ) {
       swal("Lo sentimos!", "Debes indicar un rango de fechas.", "error");
       return;
     }
@@ -102,23 +81,32 @@ export class CompetitionComponent implements OnInit {
       return;
     }
 
-    if (this.submitType === 'Guardar') {
-      this.competitionService.createCompetition(this.competitionModel).subscribe(
-        response => this.loadList(),
-        err => {
-          console.log(err.error.message === 'Error existing address');
-          if (err) {
-            swal("Lo sentimos!", "La URL ya existe", "error")
-          } else {
-            swal("Lo sentimos!", "El objeto no ha podido ser añadido.", "error")
+    if (this.submitType === "Guardar") {
+      this.competitionService
+        .createCompetition(this.competitionModel)
+        .subscribe(
+          response => this.loadList(),
+          err => {
+            console.log(err.error.message === "Error existing address");
+            if (err) {
+              swal("Lo sentimos!", "La URL ya existe", "error");
+            } else {
+              swal(
+                "Lo sentimos!",
+                "El objeto no ha podido ser añadido.",
+                "error"
+              );
+            }
           }
-        }
-      );
+        );
     } else {
-      this.competitionService.editCompetition(this.competitionModel).subscribe(
-        response => this.loadList(),
-        err => swal("Lo sentimos!", "El objeto no ha podido ser editado.", "error")
-      );
+      this.competitionService
+        .editCompetition(this.competitionModel)
+        .subscribe(
+          response => this.loadList(),
+          err =>
+            swal("Lo sentimos!", "El objeto no ha podido ser editado.", "error")
+        );
     }
     this.showNew = false;
   }
@@ -126,24 +114,38 @@ export class CompetitionComponent implements OnInit {
   onEdit(index: number) {
     this.selectedRow = index;
     this.competitionModel = Competition.empty();
-    this.competitionModel = Object.assign({}, this.competitions[this.selectedRow]);
+    this.competitionModel = Object.assign(
+      {},
+      this.competitions[this.selectedRow]
+    );
     const initDate = new Date(String(this.competitionModel.init));
     const endDate = new Date(String(this.competitionModel.end));
-    this.competitionModel.init = { day: initDate.getDate(), month: initDate.getMonth(), year: initDate.getFullYear() }
-    this.competitionModel.end = { day: endDate.getDate(), month: endDate.getMonth(), year: endDate.getFullYear() }
-    this.submitType = 'Actualizar';
+    this.competitionModel.init = {
+      day: initDate.getDate(),
+      month: initDate.getMonth(),
+      year: initDate.getFullYear()
+    };
+    this.competitionModel.end = {
+      day: endDate.getDate(),
+      month: endDate.getMonth(),
+      year: endDate.getFullYear()
+    };
+    this.submitType = "Actualizar";
     this.showNew = true;
   }
 
   onView(index: number) {
-    this.router.navigate(['competitions', this.competitions[index].id]);
+    this.router.navigate(["competitions", this.competitions[index].id]);
   }
 
   onDelete(index: number) {
     this.competitionService.deleteCompetition(index).subscribe(
-      response => swal("Listo!", "El registro ha sido eliminado!", "success").then((value) => {
-        this.loadList();
-      }),
+      response =>
+        swal("Listo!", "El registro ha sido eliminado!", "success").then(
+          value => {
+            this.loadList();
+          }
+        ),
       err => swal("Lo sentimos!", "El objeto ya ha sido borrado.", "error")
     );
   }
@@ -151,5 +153,4 @@ export class CompetitionComponent implements OnInit {
   onCancel() {
     this.showNew = false;
   }
-
 }
