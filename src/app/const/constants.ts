41e44baf23be4ec185560612c9243bf1a8f13d88
Cristian Sepulveda <sepulvec@colpatria.com>
2018-10-26 10:52:51 -0500

    add endpoint amazon

--- src/app/const/constants.ts
+++ src/app/const/constants.ts
@@ -1,5 +1,5 @@
 export const constants = {
-  PATH_VIDEOS: "/videos/videos/videos_converted/",
+  PATH_VIDEOS: "https://s3-us-west-2.amazonaws.com/4426-grupo1-videos/convertido/",
   PATH_FOTOS: "https://s3-us-west-2.amazonaws.com/4426-grupo1-videos/banner/",
-  PATH_REAL: "/videos/videos/original/",
+  PATH_REAL: "https://s3-us-west-2.amazonaws.com/4426-grupo1-videos/original/",
 };
